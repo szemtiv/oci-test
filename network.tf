806420504aff47c5b39db9ba3aa7c539e4b396e9
Tivadar Szemethy <szemtiv@users.noreply.github.com>
2022-11-10 18:03:54 +0100

    Update network.tf

--- network.tf
+++ network.tf
@@ -28,7 +28,7 @@ resource "oci_core_route_table" "ExampleRT" {
   vcn_id = "${oci_core_virtual_network.ExampleVCN.id}"
   display_name = "TFExampleRouteTable"
   route_rules {
-    cidr_block = "0.0.0.0/0"
+    destination = "0.0.0.0/0"
     network_entity_id = "${oci_core_internet_gateway.ExampleIG.id}"
   }
 }
