f8ca2c25750f69dfd4194ae8d27b5fd90d7d1b59
Tivadar Szemethy <szemtiv@users.noreply.github.com>
2022-11-10 18:25:55 +0100

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,34 +1,246 @@
-resource "oci_core_virtual_network" "ExampleVCN" {
+resource "oci_core_virtual_network" "DemoVCN" {
   cidr_block = "10.1.0.0/16"
   compartment_id = "${var.compartment_ocid}"
-  display_name = "TFExampleVCN"
-  dns_label = "tfexamplevcn"
-}
-
-resource "oci_core_subnet" "ExampleSubnet" {
-  # availability_domain = "${var.localAD}"
-  cidr_block = "10.1.20.0/24"
-  display_name = "TFExampleSubnet"
-  dns_label = "tfexamplesubnet"
-  security_list_ids = ["${oci_core_virtual_network.ExampleVCN.default_security_list_id}"]
-  compartment_id = "${var.compartment_ocid}"
-  vcn_id = "${oci_core_virtual_network.ExampleVCN.id}"
-  route_table_id = "${oci_core_route_table.ExampleRT.id}"
-  dhcp_options_id = "${oci_core_virtual_network.ExampleVCN.default_dhcp_options_id}"
+  display_name = "DemoVCN"
+  dns_label = "demo"
 }
 
 resource "oci_core_internet_gateway" "ExampleIG" {
   compartment_id = "${var.compartment_ocid}"
-  display_name = "TFExampleIG"
-  vcn_id = "${oci_core_virtual_network.ExampleVCN.id}"
+  display_name = "DemoIG"
+  enabled = "true"
+  vcn_id = "${oci_core_virtual_network.DemoVCN.id}"
 }
 
-resource "oci_core_route_table" "ExampleRT" {
+resource "oci_core_nat_gateway" "generated_oci_core_nat_gateway" {
+	compartment_id = "${var.compartment_ocid}"
+	display_name = "DemoNGW"
+  vcn_id = "${oci_core_virtual_network.DemoVCN.id}"
+}
+
+resource "oci_core_service_gateway" "generated_oci_core_service_gateway" {
+	compartment_id = "${var.compartment_ocid}"
+	display_name = "DemoSGW"
+	services {
+		service_id = "ocid1.service.oc1.eu-frankfurt-1.aaaaaaaa7ncsqkj6lkz36dehifizupyn6qjqsmtepsegs23yyntnsy7qrvea" # ???
+	}
+	vcn_id = "${oci_core_virtual_network.DemoVCN.id}"
+}
+
+resource "oci_core_route_table" "generated_oci_core_route_table" {
   compartment_id = "${var.compartment_ocid}"
-  vcn_id = "${oci_core_virtual_network.ExampleVCN.id}"
-  display_name = "TFExampleRouteTable"
-  route_rules {
-    destination = "0.0.0.0/0"
-    network_entity_id = "${oci_core_internet_gateway.ExampleIG.id}"
-  }
+	display_name = "DemoRT"
+	route_rules {
+		description = "traffic to the internet"
+		destination = "0.0.0.0/0"
+		destination_type = "CIDR_BLOCK"
+		network_entity_id = "${oci_core_nat_gateway.generated_oci_core_nat_gateway.id}"
+	}
+	route_rules {
+		description = "traffic to OCI services"
+		destination = "all-fra-services-in-oracle-services-network" # ???
+		destination_type = "SERVICE_CIDR_BLOCK"
+		network_entity_id = "${oci_core_service_gateway.generated_oci_core_service_gateway.id}"
+	}
+  vcn_id = "${oci_core_virtual_network.DemoVCN.id}"
+}
+
+resource "oci_core_default_route_table" "generated_oci_core_default_route_table" {
+	display_name = "DemoClusterRT"
+	route_rules {
+		description = "traffic to/from internet"
+		destination = "0.0.0.0/0"
+		destination_type = "CIDR_BLOCK"
+		network_entity_id = "${oci_core_internet_gateway.generated_oci_core_internet_gateway.id}"
+	}
+	manage_default_resource_id = "${oci_core_vcn.generated_oci_core_vcn.default_route_table_id}"
+}
+
+resource "oci_core_subnet" "service_lb_subnet" {
+	cidr_block = "10.0.20.0/24"
+	compartment_id = "${var.compartment_ocid}"
+	display_name = "DemoSvcLBSubnet"
+	dns_label = "lbsubbaa9b67e5" # ???
+	prohibit_public_ip_on_vnic = "false"
+	route_table_id = "${oci_core_default_route_table.generated_oci_core_default_route_table.id}"
+	security_list_ids = ["${oci_core_vcn.generated_oci_core_vcn.default_security_list_id}"]
+  vcn_id = "${oci_core_virtual_network.DemoVCN.id}"
+}
+
+resource "oci_core_subnet" "node_subnet" {
+	cidr_block = "10.0.10.0/24"
+  compartment_id = "${var.compartment_ocid}"
+	display_name = "DemoNodeSubnet"
+	dns_label = "subb6ab55460" # ???
+	prohibit_public_ip_on_vnic = "true"
+	route_table_id = "${oci_core_route_table.generated_oci_core_route_table.id}"
+	security_list_ids = ["${oci_core_security_list.node_sec_list.id}"]
+  vcn_id = "${oci_core_virtual_network.DemoVCN.id}"
+}
+
+resource "oci_core_subnet" "kubernetes_api_endpoint_subnet" {
+	cidr_block = "10.0.0.0/28"
+  compartment_id = "${var.compartment_ocid}"
+	display_name = "DemoApiEndpointSubnet"
+	dns_label = "sub1c707a834" # ???
+	prohibit_public_ip_on_vnic = "false"
+	route_table_id = "${oci_core_default_route_table.generated_oci_core_default_route_table.id}"
+	security_list_ids = ["${oci_core_security_list.kubernetes_api_endpoint_sec_list.id}"]
+  vcn_id = "${oci_core_virtual_network.DemoVCN.id}"
+}
+
+resource "oci_core_security_list" "service_lb_sec_list" {
+	compartment_id = "${var.compartment_ocid}"
+	display_name = "DemoSvcLBSecList"
+	vcn_id = "${oci_core_virtual_network.DemoVCN.id}"
+}
+
+resource "oci_core_security_list" "node_sec_list" {
+  compartment_id = "${var.compartment_ocid}"
+	display_name = "DemoClusterSecList"
+	egress_security_rules {
+		description = "Allow pods on one worker node to communicate with pods on other worker nodes"
+		destination = "10.0.10.0/24"
+		destination_type = "CIDR_BLOCK"
+		protocol = "all"
+		stateless = "false"
+	}
+	egress_security_rules {
+		description = "Access to Kubernetes API Endpoint"
+		destination = "10.0.0.0/28"
+		destination_type = "CIDR_BLOCK"
+		protocol = "6"
+		stateless = "false"
+	}
+	egress_security_rules {
+		description = "Kubernetes worker to control plane communication"
+		destination = "10.0.0.0/28"
+		destination_type = "CIDR_BLOCK"
+		protocol = "6"
+		stateless = "false"
+	}
+	egress_security_rules {
+		description = "Path discovery"
+		destination = "10.0.0.0/28"
+		destination_type = "CIDR_BLOCK"
+		icmp_options {
+			code = "4"
+			type = "3"
+		}
+		protocol = "1"
+		stateless = "false"
+	}
+	egress_security_rules {
+		description = "Allow nodes to communicate with OKE to ensure correct start-up and continued functioning"
+		destination = "all-fra-services-in-oracle-services-network" # ???
+		destination_type = "SERVICE_CIDR_BLOCK"
+		protocol = "6"
+		stateless = "false"
+	}
+	egress_security_rules {
+		description = "ICMP Access from Kubernetes Control Plane"
+		destination = "0.0.0.0/0"
+		destination_type = "CIDR_BLOCK"
+		icmp_options {
+			code = "4"
+			type = "3"
+		}
+		protocol = "1"
+		stateless = "false"
+	}
+	egress_security_rules {
+		description = "Worker Nodes access to Internet"
+		destination = "0.0.0.0/0"
+		destination_type = "CIDR_BLOCK"
+		protocol = "all"
+		stateless = "false"
+	}
+	ingress_security_rules {
+		description = "Allow pods on one worker node to communicate with pods on other worker nodes"
+		protocol = "all"
+		source = "10.0.10.0/24"
+		stateless = "false"
+	}
+	ingress_security_rules {
+		description = "Path discovery"
+		icmp_options {
+			code = "4"
+			type = "3"
+		}
+		protocol = "1"
+		source = "10.0.0.0/28"
+		stateless = "false"
+	}
+	ingress_security_rules {
+		description = "TCP access from Kubernetes Control Plane"
+		protocol = "6"
+		source = "10.0.0.0/28"
+		stateless = "false"
+	}
+	ingress_security_rules {
+		description = "Inbound SSH traffic to worker nodes"
+		protocol = "6"
+		source = "0.0.0.0/0"
+		stateless = "false"
+	}
+  vcn_id = "${oci_core_virtual_network.DemoVCN.id}"
+}
+
+resource "oci_core_security_list" "kubernetes_api_endpoint_sec_list" {
+  compartment_id = "${var.compartment_ocid}"
+	display_name = "DemoApiSvrSecList"
+	egress_security_rules {
+		description = "Allow Kubernetes Control Plane to communicate with OKE"
+		destination = "all-fra-services-in-oracle-services-network"
+		destination_type = "SERVICE_CIDR_BLOCK"
+		protocol = "6"
+		stateless = "false"
+	}
+	egress_security_rules {
+		description = "All traffic to worker nodes"
+		destination = "10.0.10.0/24"
+		destination_type = "CIDR_BLOCK"
+		protocol = "6"
+		stateless = "false"
+	}
+	egress_security_rules {
+		description = "Path discovery"
+		destination = "10.0.10.0/24"
+		destination_type = "CIDR_BLOCK"
+		icmp_options {
+			code = "4"
+			type = "3"
+		}
+		protocol = "1"
+		stateless = "false"
+	}
+	ingress_security_rules {
+		description = "External access to Kubernetes API endpoint"
+		protocol = "6"
+		source = "0.0.0.0/0"
+		stateless = "false"
+	}
+	ingress_security_rules {
+		description = "Kubernetes worker to Kubernetes API endpoint communication"
+		protocol = "6"
+		source = "10.0.10.0/24"
+		stateless = "false"
+	}
+	ingress_security_rules {
+		description = "Kubernetes worker to control plane communication"
+		protocol = "6"
+		source = "10.0.10.0/24"
+		stateless = "false"
+	}
+	ingress_security_rules {
+		description = "Path discovery"
+		icmp_options {
+			code = "4"
+			type = "3"
+		}
+		protocol = "1"
+		source = "10.0.10.0/24"
+		stateless = "false"
+	}
+  vcn_id = "${oci_core_virtual_network.DemoVCN.id}"
 }
